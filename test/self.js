85e60b82809be50d5de8b898c6d846908a7a766a
Renée Kooi <renee@kooi.me>
2017-07-01 17:26:57 +0200

    add self-test with plugin

--- test/self.js
+++ test/self.js
@@ -17,7 +17,7 @@ function unpackStream () {
   })
 }
 
-function runBundle (packer) {
+function runBundleUnpack (packer) {
   var b = browserify({ entries: path.join(__dirname, '../index') })
 
   return b.bundle()
@@ -25,22 +25,51 @@ function runBundle (packer) {
     .pipe(packer())
 }
 
-test('bundling itself with itself', function (t) {
+function runBundlePlugin (packer) {
+  var b = browserify({ entries: path.join(__dirname, '../plugin') })
+  b.plugin(packer)
+
+  return b.bundle()
+}
+
+test('bundling itself with itself: plugin edition', function (t) {
   t.plan(1)
-  runBundle(require('../index'))
-    .pipe(fs.createWriteStream(path.join(__dirname, './self.expected.js')))
+  runBundlePlugin(require('../plugin'))
+    .pipe(fs.createWriteStream(path.join(__dirname, './self.plugin.expected.js')))
     .on('finish', onbundle)
 
   function onbundle () {
-    runBundle(require('./self.expected'))
-      .pipe(fs.createWriteStream(path.join(__dirname, './self.actual.js')))
+    runBundlePlugin(require('./self.plugin.expected'))
+      .pipe(fs.createWriteStream(path.join(__dirname, './self.plugin.actual.js')))
       .on('finish', oncompare)
   }
 
   function oncompare () {
     t.is(
-      fs.readFileSync(path.join(__dirname, './self.expected.js'), 'utf8'),
-      fs.readFileSync(path.join(__dirname, './self.actual.js'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, './self.plugin.expected.js'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, './self.plugin.actual.js'), 'utf8'),
+      'flattened browserified code should have the same output as the commonjs version'
+    )
+    t.end()
+  }
+})
+
+test('bundling itself with itself: repack edition', function (t) {
+  t.plan(1)
+  runBundleUnpack(require('../index'))
+    .pipe(fs.createWriteStream(path.join(__dirname, './self.unpack.expected.js')))
+    .on('finish', onbundle)
+
+  function onbundle () {
+    runBundleUnpack(require('./self.unpack.expected'))
+      .pipe(fs.createWriteStream(path.join(__dirname, './self.unpack.actual.js')))
+      .on('finish', oncompare)
+  }
+
+  function oncompare () {
+    t.is(
+      fs.readFileSync(path.join(__dirname, './self.unpack.expected.js'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, './self.unpack.actual.js'), 'utf8'),
       'flattened browserified code should have the same output as the commonjs version'
     )
     t.end()
