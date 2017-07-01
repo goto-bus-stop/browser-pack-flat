be70c8cb741314fb2ee95516273f9f322babb51a
Renée Kooi <renee@kooi.me>
2017-07-01 20:56:11 +0200

    support exposing modules

---
+++ exposeHelper.js
@@ -0,0 +1,15 @@
+// Helper to expose a module.
+function expose (m) {
+  // A locally exposed module
+  if (expose.m.hasOwnProperty(m)) {
+    return expose.m[m]
+  }
+  // A module exposed on a previous chunk
+  if (typeof expose.r === 'function') {
+    return expose.r(m)
+  }
+  // A module exposed on a later chunk
+  if (typeof require === 'function') {
+    return require(m)
+  }
+}
---
+++ test/expose.js
@@ -0,0 +1,36 @@
+var test = require('tape')
+var path = require('path')
+var browserify = require('browserify')
+var concat = require('concat-stream')
+var vm = require('vm')
+
+test('manual exposed/external modules', function (t) {
+  var externalModule = './' + path.relative(process.cwd(), require.resolve('./expose/externalModule.js'))
+
+  var b1 = browserify({ entries: path.join(__dirname, 'expose/index.js') })
+    .plugin(require.resolve('../plugin'))
+    .external(externalModule)
+  
+  var b2 = browserify()
+    .plugin(require.resolve('../plugin'))
+    .require(externalModule, { expose: externalModule })
+
+  b1.bundle().pipe(concat(function (output1) {
+    b2.bundle().pipe(concat(function (output2) {
+      var nodeOutput = require('./expose/index.js')
+
+      var withPluginOutput = {}
+      var withPlugin = vm.createContext({
+        module: withPluginOutput
+      })
+
+      vm.runInContext(output2, withPlugin)
+      vm.runInContext(output1, withPlugin)
+
+      t.is(nodeOutput, 'localModule /// ' + process.cwd() + '/test/expose/anotherLocalModule.js')
+      t.is(withPluginOutput.exports, 'localModule /// /test/expose/anotherLocalModule.js')
+
+      t.end()
+    }))
+  }))
+})
