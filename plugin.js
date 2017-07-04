fc65f34d9608a326346cd06aab86ad9d8857bc85
Renée Kooi <renee@kooi.me>
2017-07-04 23:08:21 +0200

    actually support options in the plugin

--- plugin.js
+++ plugin.js
@@ -1,9 +1,22 @@
 var pack = require('./index')
 
 module.exports = function apply (b, opts) {
+  opts = assign(opts || {}, { raw: true })
+
   b.pipeline.get('pack').splice(0, 1,
-    pack({ standalone: b._options.standalone, raw: true })
+    pack(assign(opts, { standalone: b._options.standalone }))
   )
 
   b.on('reset', function () { apply(b, opts) })
+}
+
+function assign (base, merge) {
+  var o = {}
+  for (var i in base) if (base.hasOwnProperty(i)) {
+    o[i] = base[i]
+  }
+  for (var i in merge) if (merge.hasOwnProperty(i)) {
+    o[i] = merge[i]
+  }
+  return o
 }
