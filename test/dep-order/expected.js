c28f7a1edc90b6c5dc42044380eb9e2d805db5b4
Renée Kooi <renee@kooi.me>
2024-10-10 08:37:14 +0200

    Update snapshots

--- test/dep-order/expected.js
+++ test/dep-order/expected.js
@@ -1,19 +1,19 @@
 (function(){
 var _$one_2 = {};
-(function (__filename){
+(function (__filename){(function (){
 console.log(__filename)
 
-}).call(this,"/test/dep-order/one.js")
+}).call(this)}).call(this,"/test/dep-order/one.js")
 var _$two_4 = {};
-(function (__filename){
+(function (__filename){(function (){
 console.log(__filename)
 
-}).call(this,"/test/dep-order/two.js")
+}).call(this)}).call(this,"/test/dep-order/two.js")
 var _$three_3 = {};
-(function (__filename){
+(function (__filename){(function (){
 console.log(__filename)
 
-}).call(this,"/test/dep-order/three.js")
+}).call(this)}).call(this,"/test/dep-order/three.js")
 var _$app_1 = {};
 _$one_2
 _$two_4
--- test/lazy-order/expected.js
+++ test/lazy-order/expected.js
@@ -14,7 +14,7 @@ console.log('always')
 var _$d_5 = Math.random()
 
 var _$app_2 = {};
-(function (global){
+(function (global){(function (){
 _$a_1
 global.later = function () {
   _$b_3({}) // should not run immediately
@@ -23,5 +23,5 @@ if (_$d_5>0.5) {
   _$c_4({}) // should not always run
 }
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 }());
